381ff057d682ad3bc4b31ff6cd2837360d925970
Nadezhda Mikhonova <mihon.nadin@gmail.com>
2025-09-17 14:12:09 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+
 function App() {
+  const createStore = (reducer, initialState) => {
+    const currentReducer = reducer;
+    let state = initialState;
+    let listener = undefined;
+
+    return {
+      dispatch(action) {
+        state = currentReducer(state, action);
+        if (listener) {
+          listener();
+        }
+      },
+      subscribe(newListener) {
+        listener = newListener;
+      },
+      getState() {
+        return state;
+      }
+    }
+  };
+
+  const counter = (state = 0, action) => {
+    switch (action.type) {
+      case INCREMENT:
+        return state + 1;
+      case DECREMENT:
+        return state - 1;
+      default:
+        return state;
+    }
+  }
+
+  const store = createStore(counter);
+
+  store.subscribe(() => {
+    console.log(`Change: ${store.getState()}`)
+  });
+
+  store.dispatch({type: INCREMENT});
+  store.dispatch({type: INCREMENT});
+  store.dispatch({type: INCREMENT});
+  store.dispatch({type: INCREMENT});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
